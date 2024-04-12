324744a50a012659b358fa1289013aea87824ccd
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-12 19:04:07 +0200

    change hashed passwords and add comment

--- server/seeds/seed-users.js
+++ server/seeds/seed-users.js
@@ -1,26 +1,26 @@
 const Task = require("../models/user"); // Adjust the path to your models as necessary
 
 const users = [
-  // password: password
+  // password: password (10 rounds)
   {
     username: "Alice",
-    password: "$2a$12$TFztTSlXoGCONfpcRW752.8j.lWX932qrhmZwvVRKmSKZ.5iLGo6W",
+    password: "$2a$10$pu/5pol4J.51D9AAyUg9c.hfnVVdnuSRpYACT8DPXp3/.TD6Kv5QO",
   },
   {
     username: "Bob",
-    password: "$2a$12$TFztTSlXoGCONfpcRW752.8j.lWX932qrhmZwvVRKmSKZ.5iLGo6W",
+    password: "$2a$10$pu/5pol4J.51D9AAyUg9c.hfnVVdnuSRpYACT8DPXp3/.TD6Kv5QO",
   },
   {
     username: "Charlie",
-    password: "$2a$12$TFztTSlXoGCONfpcRW752.8j.lWX932qrhmZwvVRKmSKZ.5iLGo6W",
+    password: "$2a$10$pu/5pol4J.51D9AAyUg9c.hfnVVdnuSRpYACT8DPXp3/.TD6Kv5QO",
   },
   {
     username: "Dana",
-    password: "$2a$12$TFztTSlXoGCONfpcRW752.8j.lWX932qrhmZwvVRKmSKZ.5iLGo6W",
+    password: "$2a$10$pu/5pol4J.51D9AAyUg9c.hfnVVdnuSRpYACT8DPXp3/.TD6Kv5QO",
   },
   {
     username: "Evan",
-    password: "$2a$12$TFztTSlXoGCONfpcRW752.8j.lWX932qrhmZwvVRKmSKZ.5iLGo6W",
+    password: "$2a$10$pu/5pol4J.51D9AAyUg9c.hfnVVdnuSRpYACT8DPXp3/.TD6Kv5QO",
   },
 ];
 
