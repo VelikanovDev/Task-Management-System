d3dfaf8d2183f66774b555f36f7a13eec4f811c8
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-12 19:13:05 +0200

    add handleSubmit and states for fields

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { login } from "../services/UserService";
 
-const LoginPage = () => {
+const LoginPage = ({ isLoggedIn, setIsLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  // This useEffect will run whenever isLoggedIn changes.
+  useEffect(() => {
+    console.log("isLoggedIn state updated to: ", isLoggedIn);
+    if (isLoggedIn) {
+      navigate("/");
+    }
+  }, [isLoggedIn, navigate]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await login(username, password);
+      if (result.success) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
 
   return (
     <div>
       <h2>Login</h2>
-      <form className={"authForm"}>
+      <form className={"authForm"} onSubmit={handleSubmit}>
         <div style={{ marginBottom: "10px" }}>
-          <TextField id="login" label="Username" variant="outlined" fullWidth />
+          <TextField
+            id="login"
+            label="Username"
+            variant="outlined"
+            onChange={(e) => setUsername(e.target.value)}
+            fullWidth
+          />
         </div>
         <div style={{ marginBottom: "10px" }}>
           <TextField
             id="password"
+            type="password"
             label="Password"
             variant="outlined"
+            onChange={(e) => setPassword(e.target.value)}
             fullWidth
           />
         </div>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,35 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { register } from "../services/UserService";
+import { Alert } from "@mui/lab";
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the form from being submitted traditionally
+    if (password !== confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+
+    if (username === "" || password === "" || confirmPassword === "") {
+      setError("Username and password are required");
+      return;
+    }
+
+    const result = await register(username, password, confirmPassword);
+    if (result && result.success) {
+      setSuccess(true);
+      setError("");
+      navigate("/login", { replace: true });
+      window.location.reload();
+    } else {
+      setError(result.message || "Registration failed. Please try again.");
+      setSuccess(false);
+    }
+  };
   return (
     <div>
       <h2>Register</h2>
       <div>
-        <form className={"authForm"}>
+        <form className={"authForm"} onSubmit={handleSubmit}>
+          {error && (
+            <Alert
+              severity="error"
+              style={{ marginTop: "20px", marginBottom: "20px" }}
+              fullWidth
+            >
+              {error}
+            </Alert>
+          )}
+          {success && (
+            <Alert
+              severity="success"
+              style={{ marginTop: "20px", marginBottom: "20px" }}
+            >
+              Registration successful! Redirecting...
+            </Alert>
+          )}
           <div style={{ marginBottom: "10px" }}>
             <TextField
-              id="login"
+              id="username"
               label="Username"
               variant="outlined"
+              onChange={(e) => setUsername(e.target.value)}
               fullWidth
             />
           </div>
           <div style={{ marginBottom: "10px" }}>
             <TextField
               id="password"
+              type="password"
               label="Password"
               variant="outlined"
+              onChange={(e) => setPassword(e.target.value)}
               fullWidth
             />
           </div>
           <div style={{ marginBottom: "10px" }}>
             <TextField
               id="confirm"
+              type="password"
               label="Confirm password"
               variant="outlined"
+              onChange={(e) => setConfirmPassword(e.target.value)}
               fullWidth
             />
           </div>
