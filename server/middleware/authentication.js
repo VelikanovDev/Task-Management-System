38ecd37887d83c10137c82e76f8cb540ae9e3af7
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-14 18:51:55 +0200

    remove isLoggedIn middleware, checking it with session

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,15 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-module.exports.isLoggedIn = (req, res, next) => {
-  if (!req.session.userId) {
-    res.status(401).json({ message: "User not logged in" });
-  } else {
-    console.log("User is logged in");
-    next();
-  }
-};
-
 module.exports.areCredentialsVerified = async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
